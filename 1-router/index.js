d8d24c6ca21469120af92ce9f6a812b13f0c1da6
kimseunggyu <dev.seunggyu@gmail.com>
2022-04-24 22:19:01 +0900

    [feat] spa 기반 라우터 기능 구현 - 아주 간단한 라우팅 기능

--- 1-router/index.js
+++ 1-router/index.js
@@ -4,6 +4,21 @@ import Pages from './pages.js';
 
 const pages = Pages($('#root'));
 
+$('header > nav > ul').addEventListener('click', (event) => {
+  const { target } = event;
+
+  if (target.matches('button[data-navigate]')) {
+    const { navigate } = target.dataset;
+
+    if (navigate === '/1-router/') {
+      pages.home();
+    } else if (navigate === '/1-router/posts') {
+      pages.posts();
+    } else {
+      pages.notFound();
+    }
+  }
+});
+
+// 첫 페이지 렌더링
 pages.home();
-// pages.posts();
-// pages.notFound();
