abec301e4d9d2b90ab004569436fd5e56ad16ea0
kimseunggyu <dev.seunggyu@gmail.com>
2022-04-25 00:44:11 +0900

    [refactor] router 기본 구조 변경

--- 1-router/index.js
+++ 1-router/index.js
@@ -2,9 +2,7 @@ import { $ } from './utils/dom.js';
 
 import Pages from './pages.js';
 
-import Router from './router.js';
-
-const router = Router();
+import router from './router.js';
 
 const pages = Pages($('#root'));
 
@@ -14,19 +12,12 @@ $('header > nav > ul').addEventListener('click', (event) => {
   if (target.matches('button[data-navigate]')) {
     const { navigate } = target.dataset;
 
-    // router.navigate('aaa');
-    router.route(navigate);
+    router.changeRoute(navigate);
   }
 });
-
-// 첫 페이지 렌더링
-pages.home();
 
 // router 추가
 router.addRoute('/1-router/', pages.home);
 router.addRoute('/1-router/posts', pages.posts);
 router.setNotFound(pages.notFound);
-// router.addRoute('/1-router/bad-url', pages.notFound);
-
-// console.log(router);
-//
+router.route();
--- 1-router/router.js
+++ 1-router/router.js
@@ -1,28 +1,35 @@
-export default () => {
-  const routes = [];
-  const router = {};
-  let notFoundComponent = () => {};
+const CHANGE_ROUTE_EVENT = 'urlChange';
 
-  router.addRoute = (path, component) => {
-    routes.push({
+const router = {
+  routes: [],
+  notFoundComponent: () => {},
+  setNotFound: (component) => {
+    router.notFoundComponent = component;
+  },
+  addRoute: (path, component) => {
+    router.routes.push({
       path,
       component,
     });
-  };
+  },
+  changeRoute: (url) => {
+    history.pushState(null, null, url);
+    window.dispatchEvent(new CustomEvent(CHANGE_ROUTE_EVENT));
+  },
 
-  router.setNotFound = (component) => {
-    notFoundComponent = component;
-  };
+  route: () => {
+    const { pathname } = location;
 
-  router.route = (path) => {
-    const currentRouter = routes.find((value) => value.path === path);
+    const currentRouter = router.routes.find((value) => value.path === pathname);
     if (!currentRouter) {
-      notFoundComponent();
+      router.notFoundComponent();
       return;
     }
-
     currentRouter.component();
-  };
-
-  return router;
+  },
 };
+
+window.addEventListener(CHANGE_ROUTE_EVENT, router.route);
+window.addEventListener('popstate', router.route); // 뒤로 가기 이벤트 등록
+
+export default router;
