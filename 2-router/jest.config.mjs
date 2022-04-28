e41c44e1fd64e3109e2d9a19ea9b655ecd35219e
kimseunggyu <dev.seunggyu@gmail.com>
2022-04-29 00:03:22 +0900

    [chore] jsdom 및 context 테스트 설정 - 브라우저 환경을 테스트 하기 위한 jsdom 패키지 설치 - context 문법을 사용하기 위해 context 설정

--- 2-router/jest.config.mjs
+++ 2-router/jest.config.mjs
@@ -128,7 +128,7 @@ export default {
   // setupFiles: ['dotenv/config'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['jest-plugin-context/setup'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -138,6 +138,7 @@ export default {
 
   // The test environment that will be used for testing
   // testEnvironment: "jest-environment-node",
+  testEnvironment: 'jsdom',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
