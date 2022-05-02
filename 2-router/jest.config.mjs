64037b433af670c2c4d046ad7b04f28c39b08e36
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-05-02 15:41:04 +0900

    [chore] types.ts 파일 커버리지에서 제외

--- 2-router/jest.config.mjs
+++ 2-router/jest.config.mjs
@@ -26,9 +26,7 @@ export default {
   // coverageDirectory: undefined,
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: ['/node_modules/', 'types.ts'],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
