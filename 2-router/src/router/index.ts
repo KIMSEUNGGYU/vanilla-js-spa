c413f68e84d0a253f840f7dc9a9998b2db384184
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-05-02 15:38:24 +0900

    [refactor] router 모듈화 - route 함수의 query, params 가져오는 로직  분리

--- 2-router/src/router/index.ts
+++ 2-router/src/router/index.ts
@@ -10,7 +10,7 @@ interface RouterImpl {
 
 type RouterType = {
   testRegExp: RegExp;
-  component: (params?: any) => void;
+  component: (location?: any) => void;
   params: string[];
 };
 export default class Router implements RouterImpl {
@@ -22,14 +22,44 @@ export default class Router implements RouterImpl {
     window.addEventListener('popstate', this.route); // 뒤로 가기 이벤트 등록
   }
 
-  notFoundComponent: () => void;
+  // ❓ THINK : (jest에서) private 인데 어떻게 접근이 가능하지? (브라우저는 접근 불가)
+  private notFoundComponent: () => void;
+
+  private getUrlQuery = () => {
+    const searchQuery = new URLSearchParams(window.location.search);
+    const query: Obj = {};
+    for (const [key, value] of searchQuery) {
+      query[key] = value;
+    }
+
+    return query;
+  };
+
+  private getUrlParams = (currentRouter: RouterType, pathname: string) => {
+    const params: Obj = {};
+
+    if (currentRouter.params.length !== 0) {
+      const matches = pathname.match(currentRouter.testRegExp);
+      // ❓ THINK : 테스트 커버리지를 검사할 때  matches?.shift 쓰면 안되어서 if문 안으로 검사함.
+      // 이럴 경우 어떻게 처리하는게 좋을까?
+      if (matches) {
+        matches.shift();
+        matches.forEach((paramValue, index) => {
+          const paramName = currentRouter.params[index];
+          params[paramName] = paramValue;
+        });
+      }
+    }
+
+    return params;
+  };
 
   setNotFound = (component: () => void): Router => {
     this.notFoundComponent = component;
     return this;
   };
 
-  addRoute = (path: string, component: (params?: any) => void): Router => {
+  addRoute = (path: string, component: () => void): Router => {
     // params 제공
     const params: string[] = [];
 
@@ -56,14 +86,10 @@ export default class Router implements RouterImpl {
   };
 
   route = (): void => {
-    const { pathname } = location;
+    const { pathname } = window.location;
 
-    // query
-    const searchQuery = new URLSearchParams(window.location.search);
-    const query: Obj = {};
-    for (const [key, value] of searchQuery) {
-      query[key] = value;
-    }
+    // parse query
+    const query = this.getUrlQuery();
 
     const currentRouter = this.router.find((route) => route.testRegExp.test(pathname));
     if (!currentRouter) {
@@ -72,25 +98,13 @@ export default class Router implements RouterImpl {
     }
 
     // parse params
-    const urlParams: Obj = {};
-    if (currentRouter.params.length !== 0) {
-      const matches = pathname.match(currentRouter.testRegExp);
-      // ❓ THINK : 테스트 커버리지를 검사할 때  matches?.shift 쓰면 안되어서 if문 안으로 검사함.
-      // 이럴 경우 어떻게 처리하는게 좋을까?
-      if (matches) {
-        matches.shift();
-        matches.forEach((paramValue, index) => {
-          const paramName = currentRouter.params[index];
-          urlParams[paramName] = paramValue;
-        });
-      }
-    }
+    const params = this.getUrlParams(currentRouter, pathname);
 
-    const _location = {
-      params: urlParams,
+    const location = {
+      params,
       query,
     };
 
-    currentRouter.component(_location);
+    currentRouter.component(location);
   };
 }
