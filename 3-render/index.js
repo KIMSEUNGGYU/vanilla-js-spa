cd90bec33437b58a7096840e46ddc978cf80c0d2
kimseunggyu <dev.seunggyu@gmail.com>
2022-05-16 20:59:28 +0900

    [feat] 레지시트리 기능 구현 - 레지시트리는 앱에서 사용할 수 있는 모든 구성요소

--- 3-render/index.js
+++ 3-render/index.js
@@ -2,16 +2,25 @@ import { $ } from './src/utils/dom.js';
 
 import getTodos from './src/getTodos.js';
 
-import appView from './src/views/app.js';
+// import appView from './src/views/app.js';
+import todosView from './src/views/todos.js';
+import counterView from './src/views/couter.js';
+import filtersView from './src/views/filters.js';
+
+import registry from './src/registry.js';
+
+registry.add({ name: 'todos', component: todosView });
+registry.add({ name: 'counter', component: counterView });
+registry.add({ name: 'filters', component: filtersView });
 
 const state = {
   todos: getTodos(), // todo[]:  { text: '', completed: boolean}
   currentFilter: 'All', // All, Active, Completed
 };
 
-const $main = $('.todoapp');
-
 window.requestAnimationFrame(() => {
-  const $newMain = appView({ $target: $main, state }); // $main을 복사해서 적용된 DOM 을 반환
+  const $main = $('.todoapp');
+  // const $newMain = appView({ $target: $main, state }); // $main을 복사해서 적용된 DOM 을 반환
+  const $newMain = registry.renderRoot({ $target: $main, state }); // $main을 복사해서 적용된 DOM 을 반환
   $main.replaceWith($newMain); // 적용된 DOM 으로 대체
 });
---
+++ 3-render/src/registry.js
@@ -0,0 +1,42 @@
+import { $$ } from './utils/dom.js';
+
+// 레지시트리는 앱에서 사용할 수 있는 모든 구성요소(컴포넌트)
+const registry = {};
+
+const renderWrapper = ({ component }) => {
+  return ({ $target, state }) => {
+    const $element = component({ $target, state });
+
+    const $childComponents = $$('[data-component]', $element);
+
+    Array.from($childComponents).forEach((target) => {
+      const name = target.dataset.component;
+
+      const child = registry[name];
+      if (!child) {
+        return;
+      }
+
+      target.replaceWith(child({ $target: target, state }));
+    });
+
+    return $element;
+  };
+};
+
+const add = ({ name, component }) => {
+  registry[name] = renderWrapper({ component });
+};
+
+const renderRoot = ({ $target, state }) => {
+  const cloneComponent = ({ $target }) => {
+    return $target.cloneNode(true);
+  };
+
+  return renderWrapper({ component: cloneComponent })({ $target, state });
+};
+
+export default {
+  add,
+  renderRoot,
+};
